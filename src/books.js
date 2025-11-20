c5ebc063afa66034303cdcc8df9ab077d1fa64ed
Andrey Osokin <osokinpochta@gmail.com>
2025-11-19 20:40:00 -0500

    lecture 47

---
+++ src/books.js
@@ -0,0 +1,31 @@
+export const books = [
+  {
+    author: "Mel Robbins, Sawyer Robbins",
+    title: "The Let Them Theory",
+    img: "./Images/book_01.jpg",
+    id: 1,
+  },
+
+  {
+    author: "John Kennedy",
+    title: "How to Test Negative for Stupid",
+    img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SL1500_.jpg",
+    id: 2,
+  },
+
+  {
+    author: "John Grisham",
+    title: "The Widow: A Novel",
+    img: "https://m.media-amazon.com/images/I/81CWXchKvRL._SL1500_.jpg",
+    id: 3,
+  },
+
+  {
+    author: "Virginia Roberts Giuffre",
+    title: "Nobody's Girl: A Memoir of Surviving Abuse",
+    img: "https://m.media-amazon.com/images/I/61KzpshHPZL._SL1500_.jpg",
+    id: 4,
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+import { books } from "./books";
+import Book from "./Book";
 // const img = "./Images/book_01.jpg";
 // const author = "Mel Robbins, Sawyer Robbins";
 // const title = "The Let Them Theory";
-
-const books = [
-  {
-    author: "Mel Robbins, Sawyer Robbins",
-    title: "The Let Them Theory",
-    img: "./Images/book_01.jpg",
-    id: 1,
-  },
-
-  {
-    author: "John Kennedy",
-    title: "How to Test Negative for Stupid",
-    img: "https://m.media-amazon.com/images/I/71tbImx2YVL._SL1500_.jpg",
-    id: 2,
-  },
-
-  {
-    author: "John Grisham",
-    title: "The Widow: A Novel",
-    img: "https://m.media-amazon.com/images/I/81CWXchKvRL._SL1500_.jpg",
-    id: 3,
-  },
-
-  {
-    author: "Virginia Roberts Giuffre",
-    title: "Nobody's Girl: A Memoir of Surviving Abuse",
-    img: "https://m.media-amazon.com/images/I/61KzpshHPZL._SL1500_.jpg",
-    id: 4,
-  },
-];
 
 // const someFunc = (param1, param2) => {
 //   console.log(param1, param2);
@@ -55,23 +26,10 @@ const BookList = () => {
   return (
     <section className="bookList">
       {books.map((book) => {
-        console.log(book);
-        return <Book book={book} key={book.id} />;
+        //console.log(book);
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  console.log(props);
-  const { img, title, author } = props.book;
-
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
